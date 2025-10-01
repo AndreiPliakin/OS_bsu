95f6be9d7a8e4baf723b02325d4e4d6423646fca
andrei <destroyer.bainbridge1@gmail.com>
2025-10-01 18:01:38 +0300

    lab_1_linux_done

---
+++ lab_1_linux/build/CMakeFiles/Number.dir/Number/Number.cpp.o.d
@@ -0,0 +1,4 @@
+CMakeFiles/Number.dir/Number/Number.cpp.o: \
+ /home/andrei/Andrei/OS_bsu/lab_1_linux/Number/Number.cpp \
+ /usr/include/stdc-predef.h \
+ /home/andrei/Andrei/OS_bsu/lab_1_linux/Number/Number.h
---
+++ lab_1_linux/build/CMakeFiles/Number.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Number.dir/Number/Number.cpp.o"
+  "CMakeFiles/Number.dir/Number/Number.cpp.o.d"
+  "bin/libNumber.a"
+  "bin/libNumber.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Number.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
